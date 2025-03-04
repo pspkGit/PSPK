e22af8e039d45c3cb2afedee9cd5f94dde2266ad
pavan <saieic4@gmail.com>
2025-03-04 23:30:34 +0530

    conflict

--- Login.java
+++ Login.java
@@ -1,6 +1,5 @@
 public class Login{
     public static void main(String[] args) {
-        System.out.println("SAI RAM");
-        System.out.println("Login code");
+        System.out.println("SAI RAM"); 
     }
 }
